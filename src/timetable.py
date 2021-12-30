9542afc3c22dc6adb2c2b78f8c59ba6d38de7aa0
smileyface12349 <josh.humphriss.signup@gmail.com>
2021-12-30 17:10:29 +0000

    Progress on class allocation algorithm

--- src/timetable.py
+++ src/timetable.py
@@ -33,13 +33,77 @@ class Timetabler:
         self.classes.allocate()
 
 
+class Choices:
+    """Represents a list of student choices"""
+
+    def __init__(self):
+        self.by_student = {}  # student: [subjects]
+        self.by_subject = {}  # subject: [students]
+
+    def from_dict_by_student(self, by_student):
+        """Helper method in case the dict form has already been generated"""
+        self.by_student = by_student
+        self._update_by_subject()
+
+    def _update_by_subject(self):
+        """This should be called every time the value changes"""
+        self.by_subject = {}  # {subject: [student]}
+        for student in self.by_student:
+            subjects = self.by_student[student]
+            for subject in subjects:
+                if subject in self.by_subject:
+                    self.by_subject[subject].append(student)
+                else:
+                    self.by_subject[subject] = [student]
+
+    def add_student(self, student, *subjects):
+        """Adds a new student choice"""
+        if student in self.by_student:
+            self.by_student[student] += list(subjects)
+        else:
+            self.by_student[student] = list(subjects)
+        self._update_by_subject()
+
+
 class Classes:
     """Represents allocated classes
 
     Can be accessed to get the students in each class, or which classes conflict with each other"""
 
-    def allocate(self):
+    MAX_SIZE = 24
+    MIN_SIZE = 4
+    MAIN_BLOCKS = 4
+
+    def __init__(self):
+        self._main_blocks = [{'classes': [], 'conflicts': {}}]*self.MAIN_BLOCKS
+        self._overflow_blocks = []
+
+    def allocate(self, choices:Choices):
         """Allocates or re-allocates students to classes given their choices"""
+
+        working_main_blocks = [{'classes': [], 'conflicts': {}}]*self.MAIN_BLOCKS
+        working_overflow_blocks = []
+
+        subjects_not_considered = choices.by_subject
+
+        self.allocate_rec(subjects_not_considered, working_main_blocks, working_overflow_blocks)
+
+    def allocate_rec(self, subjects_not_considered, working_main_blocks, working_overflow_blocks, best_possibility=None):
+        if not subjects_not_considered:
+            return
+
+        possibilities = []
+
+        # consider just the first subject - other subjects will be considered on future function calls
+        subject = dict(subjects_not_considered).pop(0) # here, dict() ensures the parameter is passed by value, not by reference
+        for block in self._main_blocks:
+            # TODO: consider allocating subject to that block, resolving conflicts in any way that seems appropriate
+            p_working_main_blocks = working_main_blocks
+            p_working_overflow_blocks = working_overflow_blocks
+            possibilities.append(self.allocate_rec(subjects_not_considered, p_working_main_blocks, p_working_overflow_blocks))
+
+        # TODO: get best possibility
+        return best_possibility
 
 
 class LiveTimetable:
